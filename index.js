21efe5eabb893da0f67089268e54c723802c6858
Saad <mohdsaad88842@gmail.com>
2023-07-12 19:12:54 +0530

    1

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 
 document.querySelector("button").addEventListener("click", main)
-function main(){
+function main() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1
-var randomNumber2 = Math.floor(Math.random() * 6) + 1
-// var p1=document.querySelector(".refresh")
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1
 
-document.querySelector(".dice1 img").setAttribute("src", "dice/dice" + randomNumber1 + ".png");
-document.querySelector(".dice2 img").setAttribute("src", "dice/dice" + randomNumber2 + ".png");
+    document.querySelector(".dice1 img").setAttribute("src", "dice/dice" + randomNumber1 + ".png");
+    document.querySelector(".dice2 img").setAttribute("src", "dice/dice" + randomNumber2 + ".png");
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector(".refresh").innerHTML = "Player 1 wins!"
-}
-else if(randomNumber1==randomNumber2){
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector(".refresh").innerHTML = "You won!"
+    }
+    else if (randomNumber1 == randomNumber2) {
 
-    document.querySelector(".refresh").innerHTML = "Draw!"
-}
-else {
-    document.querySelector(".refresh").innerHTML = "Player 2 wins!"
+        document.querySelector(".refresh").innerHTML = "Draw!"
+    }
+    else {
+        document.querySelector(".refresh").innerHTML = "Your friend won!"
 
-}
+    }
 }
